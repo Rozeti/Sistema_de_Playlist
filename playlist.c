c7b886e36cf86a5fd472c96ef90d1f335402d08a
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2024-06-06 16:59:44 -0300

    Adição das funções inicializarListaOrdenada e inserirNaListaOrdenada

--- playlist.c
+++ playlist.c
@@ -11,3 +11,25 @@ Musica* criarMusica(char* artista, char* nome) {
     nova->proxima = nova->anterior = nova;
     return nova;
 }
+
+void inicializarListaOrdenada(ListaOrdenada *listaOrdenada) {
+    listaOrdenada->cabeca = NULL;
+}
+
+void inserirNaListaOrdenada(ListaOrdenada *listaOrdenada, Musica *musica) {
+    NoOrdenado *novoNo = (NoOrdenado*)malloc(sizeof(NoOrdenado));
+    novoNo->musica = musica;
+    novoNo->proximo = NULL;
+
+    if (!listaOrdenada->cabeca || strcmp(musica->nome, listaOrdenada->cabeca->musica->nome) < 0) {
+        novoNo->proximo = listaOrdenada->cabeca;
+        listaOrdenada->cabeca = novoNo;
+    } else {
+        NoOrdenado *atual = listaOrdenada->cabeca;
+        while (atual->proximo && strcmp(musica->nome, atual->proximo->musica->nome) >= 0) {
+            atual = atual->proximo;
+        }
+        novoNo->proximo = atual->proximo;
+        atual->proximo = novoNo;
+    }
+}
