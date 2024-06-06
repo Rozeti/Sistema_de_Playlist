75394437475e666e78ea3b14082535c0e606ee12
Mateus Tirulli Rozeti <129351985+Rozeti@users.noreply.github.com>
2024-06-06 17:01:04 -0300

    Inclusão das funções salvarNoArquivo e CarregarMusicas

--- playlist.c
+++ playlist.c
@@ -33,3 +33,46 @@ void inserirNaListaOrdenada(ListaOrdenada *listaOrdenada, Musica *musica) {
         atual->proximo = novoNo;
     }
 }
+
+void salvarNoArquivo(Playlist *playlist, const char* nomeArquivo) {
+    FILE *arquivo = fopen(nomeArquivo, "w");
+    if (!arquivo) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    Musica *atual = playlist->cabeca;
+    if (atual) {
+        do {
+            fprintf(arquivo, "%s;%s\n", atual->artista, atual->nome);
+            atual = atual->proxima;
+        } while (atual != playlist->cabeca);
+    }
+    fclose(arquivo);
+}
+
+void carregarMusicas(Playlist *playlist, const char* nomeArquivo, ListaOrdenada *listaOrdenada) {
+    FILE *arquivo = fopen(nomeArquivo, "r");
+    if (!arquivo) {
+        printf("Erro ao abrir o arquivo.\n");
+        return;
+    }
+    char linha[MAX_LEN * 2];
+    while (fgets(linha, sizeof(linha), arquivo)) {
+        char *artista = strtok(linha, ";");
+        char *nome = strtok(NULL, "\n");
+        if (artista && nome) {
+            Musica *nova = criarMusica(artista, nome);
+            inserirNaListaOrdenada(listaOrdenada, nova);
+            if (!playlist->cabeca) {
+                playlist->cabeca = nova;
+            } else {
+                Musica* cauda = playlist->cabeca->anterior;
+                cauda->proxima = nova;
+                nova->anterior = cauda;
+                nova->proxima = playlist->cabeca;
+                playlist->cabeca->anterior = nova;
+            }
+        }
+    }
+    fclose(arquivo);
+}
